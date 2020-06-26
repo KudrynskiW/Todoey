48d6043ba912e057700c5cbe05e659764b46e8c2
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-06-26 15:00:52 +0200

    Core Data manually added to project

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         return true
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        self.saveContext()
+    }
+
+    // MARK: Persistent Container
+    
+    lazy var persistenContainer: NSPersistentContainer  = {
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistenContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
     // MARK: UISceneSession Lifecycle
